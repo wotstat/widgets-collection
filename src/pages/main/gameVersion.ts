94522734bfde67b20c459159dbc39ebf953bb195
Andrei <soprachev@mail.ru>
2025-06-13 07:22:29 +0300

    fix

--- src/pages/main/gameVersion.ts
+++ src/pages/main/gameVersion.ts
@@ -1,12 +1,12 @@
 import { computedAsync } from "@vueuse/core";
 
-async function parseGameVersion(url: string) {
+async function parseGameVersion(url: string, extension: '.wotmod' | '.mtmod' = '.wotmod') {
   const response = await fetch(url)
   const text = await response.text();
   const parser = new DOMParser();
   const doc = parser.parseFromString(text, "text/xml");
-  return doc.documentElement.querySelector("Path[mask='*.wotmod']")?.textContent?.replace('./mods/', '') ?? 'GAME_VERSION';
+  return doc.documentElement.querySelector(`Path[mask='*${extension}']`)?.textContent?.replace('./mods/', '') ?? 'GAME_VERSION';
 }
 
-export const gameVersionRU = computedAsync(async () => await parseGameVersion("https://raw.githubusercontent.com/IzeBerg/wot-src/RU/sources/paths.xml"), './mods/1.20.0.0');
+export const gameVersionRU = computedAsync(async () => await parseGameVersion("https://raw.githubusercontent.com/IzeBerg/wot-src/RU/sources/paths.xml", '.mtmod'), './mods/1.20.0.0');
 export const gameVersionEU = computedAsync(async () => await parseGameVersion("https://raw.githubusercontent.com/IzeBerg/wot-src/EU/sources/paths.xml"), './mods/1.20.0.0');
