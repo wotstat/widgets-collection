c803acb2d6d9d9a443b4b58e54e3b77fd947807e
Andrei Soprachev <13734096+SoprachevAK@users.noreply.github.com>
2026-09-04 19:12:36 +0300

    fix

--- src/components/comp7/utils.ts
+++ src/components/comp7/utils.ts
@@ -106,8 +106,8 @@ function getRankImageName(rank: RankImageDefinition, game: GameVendor = 'mt'): s
   else division = rank as Division
 
   if (division == 'qual') return 'qualification'
-  if (division == 'sixth') return 'sixth_logo'
-  if (division == 'fifth') return 'fifth_logo'
+  if (division == 'sixth' && game == 'mt') return 'sixth_logo'
+  if (division == 'fifth' && game == 'mt') return 'fifth_logo'
   return division
 }
 
